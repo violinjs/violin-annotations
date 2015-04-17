dd5b89fdffaf4ae666f01fe89d5f838e01004c0a
Jonathan ARNAULT <jonathan.arnault@starburst-computing.com>
2015-04-17 16:05:10 +0200

    Create Annotation abstract class

---
+++ src/annotation/Annotation.js
@@ -0,0 +1,34 @@
+var path = require("path");
+
+var Target = require(path.join(__dirname, "..", "Target.js"))
+
+/**
+ * Abstract annotation
+ * @constructor
+ */
+var Annotation = function (parameters) {
+
+};
+
+/**
+ * Get annotation targets
+ * @returns {Target[]}
+ */
+Annotation.prototype.getTargets = function () {
+    return [
+        Target.CLASS_ANNOTATION,
+        Target.METHOD_ANNOTATION,
+        Target.PROPERTY_ANNOTATION
+    ];
+};
+
+/**
+ * Get annotation name
+ * @abstract
+ * @returns {String}
+ */
+Annotation.prototype.getName = function () {
+
+};
+
+module.exports = Annotation;
---
+++ test/annotation/Annotation.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+var Annotation = require(path.join(__dirname, "..", "..", "src", "annotation", "Annotation.js"));
+
+
+describe("Annotation", function () {
+    var annotation = new Annotation();
+
+    describe("#getTarget()", function () {
+        it("should return an Array", function () {
+            annotation.getTargets().should.be.an.Array;
+        });
+    });
+
+    describe("#getName()", function () {
+
+        it("should be an abstract method", function () {
+            (annotation.getName() === undefined).should.be.true;
+        });
+    });
+});
